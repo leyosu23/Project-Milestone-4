8c9452333b3a2dd7f611070414d88efb444cb013
DESKTOP-I2IQFV7\User <leyosu23@gmail.com>
2026-04-18 03:47:16 -0400

    Update fix

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from src.services import LibraryService
 import sys
 
 def demo():
-<<<<<<< HEAD
-    print("=" * 1)
-=======
     print("=" * 2)
->>>>>>> develop2
     print("BookKeeper - Library Management System")
     print("Architecture: 4+1 View Model Implementation")
     print("=" * 60)
